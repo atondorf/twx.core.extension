a8311f5732ca779ba6c2ca32faf8fe6f3f7418bd
Andreas Tondorf <atondorf@sig.biz>
2023-06-23 10:34:50 +0200

    Changes to be committed: modified:   app/src/main/java/twx/core/App.java renamed:    extension/src/main/java/twx/core/db/IDatabase.java -> extension/src/main/java/twx/core/db/IDatabaseHandler.java new file:   extension/src/main/java/twx/core/db/imp/AbstractDatabaseHandler.java modified:   extension/src/main/java/twx/core/db/imp/DBUtil.java new file:   extension/src/main/java/twx/core/db/imp/MsSQLDatabaseHandler.java new file:   extension/src/main/java/twx/core/db/model/DBModelManager.java renamed:    extension/src/main/java/twx/core/db/imp/DbAbstract.java -> extension/src/main/java/twx/core/db/model/DBModelUtil.java modified:   extension/src/main/java/twx/core/db/model/DbModel.java new file:   extension/src/main/java/twx/core/db/scriptable/DBCallableStatement.java modified:   extension/src/main/java/twx/core/db/scriptable/DBConnection.java modified:   extension/src/main/java/twx/core/db/scriptable/DBPreparedStatement.java modified:   extension/src/main/java/twx/core/db/scriptable/DBStatement.java renamed:    extension/src/main/java/twx/core/impl/DataShapeUtil.java -> extension/src/main/java/twx/core/imp/DataShapeUtil.java renamed:    extension/src/main/java/twx/core/impl/ThingUtil.java -> extension/src/main/java/twx/core/imp/ThingUtil.java

--- extension/src/main/java/twx/core/db/scriptable/DBConnection.java
+++ extension/src/main/java/twx/core/db/scriptable/DBConnection.java
@@ -14,6 +14,8 @@ import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.annotations.JSConstructor;
 import org.mozilla.javascript.annotations.JSFunction;
+import org.mozilla.javascript.annotations.JSGetter;
+import org.mozilla.javascript.annotations.JSSetter;
 
 import com.thingworx.common.exceptions.ThingworxRuntimeException;
 import com.thingworx.things.database.AbstractDatabase;
@@ -21,35 +23,24 @@ import com.thingworx.things.database.AbstractDatabase;
 import twx.core.db.imp.DBUtil;
 
 public class DBConnection extends ScriptableObject {
+    // region ScriptableObject basics
+    // --------------------------------------------------------------------------------
     private static final long serialVersionUID = 1L;
 
     public DBConnection() {}
 
-    @JSConstructor
-    public static Scriptable jsConstructor(Context cx, Object[] args, Function ctorObj, boolean inNewExpr) {
-        DBConnection result = new DBConnection();
-        if (args.length < 1 || args[0] == Context.getUndefinedValue())
-            throw new IllegalArgumentException("DBConnection - First Param must be Name of an DBThing");
-
-        String thingName = Context.toString(args[0]);
-        result.databaseThing = DBUtil.getAbstractDatabaseDirect(thingName);
-
-        if (result.databaseThing == null)
-            throw new ThingworxRuntimeException("DBConnection - " + thingName + " is not a database");
-
-        if (args.length >= 2 && args[1] != Context.getUndefinedValue())
-            result.autoCommit = Context.toBoolean(args[1]);
-
-        return result;
+    public DBConnection(String thingName) throws Exception {
+        this.databaseThing = DBUtil.getAbstractDatabaseDirect(thingName);
+        this.databaseThing.beginTransaction();
+        this.connection = this.databaseThing.getConnection();
+        this.connection.setAutoCommit(false);
     }
 
-    @JSConstructor
-    public DBConnection(String dbThingName, Boolean autoCommit) throws Exception {
-        this.databaseThing = DBUtil.getAbstractDatabaseDirect(dbThingName);
-        if (this.databaseThing == null)
-            throw new ThingworxRuntimeException("Thing:" + dbThingName + " is not a database");
-        connection = databaseThing.getConnection();
-        connection.setAutoCommit(autoCommit);
+    public DBConnection(String thingName, Boolean autoCommit) throws Exception {
+        this.databaseThing = DBUtil.getAbstractDatabaseDirect(thingName);
+        this.databaseThing.beginTransaction();
+        this.connection = this.databaseThing.getConnection();
+        this.connection.setAutoCommit(autoCommit);
     }
 
     @Override
@@ -59,31 +50,13 @@ public class DBConnection extends ScriptableObject {
 
     @Override
     protected void finalize() throws Throwable {
-        if (connection != null)
-            connection.close();
-        connection = null;
+        this.close();
+        this.databaseThing = null;
     }
-
-    protected AbstractDatabase getAbstractDatabase() throws Exception {
-        return this.databaseThing;
-    }
-
-    protected Connection getConnection() throws Exception {
-        if (this.connection == null)
-            this.connection = this.getAbstractDatabase().getConnection();
-        return this.connection;
-    }
-
-    protected DatabaseMetaData getMetaData() throws Exception {
-        return getConnection().getMetaData();
-    }
-
-    @JSFunction
-    public Boolean isAutoCommit() {
-        return this.autoCommit;
-    }
-
-    @JSFunction
+    // endregion 
+    // region Thing and DB - Information
+    // --------------------------------------------------------------------------------
+     @JSFunction
     public String getThingName() throws Exception {
         if (databaseThing == null)
             return "UNDEFINED";
@@ -92,68 +65,57 @@ public class DBConnection extends ScriptableObject {
 
     @JSFunction
     public String getCatalog() throws Exception {
-        return getConnection().getCatalog();
+        return this.connection.getCatalog();
     }
-
+    // endregion 
+    // region Connection & Transaction Handling 
+    // --------------------------------------------------------------------------------
     @JSFunction
-    public JSONObject getSpec() throws Exception {
-        JSONObject obj = new JSONObject();
-        var meta = getMetaData();
-        obj.put("dbProductName", meta.getDatabaseProductName());
-        obj.put("dbProductVersion", meta.getDatabaseProductVersion());
-        obj.put("dbDriverName", meta.getDriverName());
-        obj.put("dbDriverVersion", meta.getDriverVersion());
-        return obj;
-    }
-
-    @JSFunction
-    public JSONArray getSchemas() throws Exception {
-        JSONArray arr = new JSONArray();
-        ResultSet rs = getMetaData().getSchemas();
-        while (rs.next()) {
-            String schemaName = rs.getString("TABLE_SCHEM");
-            arr.put(schemaName);
-        }
-        return arr;
-    }
-
-    @JSFunction
-    public JSONArray getTables(String schema) throws Exception {
-        JSONArray arr = new JSONArray();
-        ResultSet rs = getMetaData().getTables(null, schema, null, null);
-        while (rs.next()) {
-            arr.put(rs.getString("TABLE_NAME"));
-        }
-        return arr;
-    }
-
-    @JSFunction
-    public JSONArray getColumns(String schema, String table) throws Exception {
-        JSONArray arr = new JSONArray();
-        ResultSet rs = getMetaData().getColumns(null, schema, table, null);
-        while (rs.next()) {
-            arr.put(rs.getString("COLUMN_NAME"));
-        }
-        return arr;
-    }
-
-    @JSFunction
-    public void commit() throws Exception {
-        getAbstractDatabase().commit(connection);
-    }
-
-    @JSFunction
-    public void rollback() throws Exception {
-        getAbstractDatabase().rollback(connection);
+    public Boolean isClosed() throws SQLException {
+        if( this.connection == null )
+            return true;
+        return this.connection.isClosed();
     }
 
     @JSFunction
     public void close() throws Exception {
-        if (connection != null)
-            connection.close();
-        connection = null;
+        if( this.connection == null )
+            return;
+        this.databaseThing.endTransaction(this.connection);
+        this.connection = null;
     }
 
+    @JSFunction
+    public void open(Boolean autoCommit) throws Exception {
+        if( this.connection != null )
+            return;
+        this.databaseThing.beginTransaction();
+        this.connection = this.databaseThing.getConnection();
+        this.connection.setAutoCommit(autoCommit);        
+    }
+
+    @JSFunction
+    public void commit() throws Exception {
+        this.databaseThing.commit(this.connection);
+    }
+
+    @JSFunction
+    public void rollback() throws Exception {
+        this.databaseThing.rollback(this.connection);
+    }
+
+    @JSFunction
+    public Boolean isAutoCommit() throws SQLException {
+        return this.connection.getAutoCommit();
+    }
+
+    @JSFunction
+    public void setAutoCommit(Boolean autoCommit) throws SQLException {
+        this.connection.setAutoCommit(autoCommit);
+    };
+    // endregion 
+    // region Statements 
+    // --------------------------------------------------------------------------------
     @JSFunction
     public static Object createStatement(Context cx, Scriptable me, Object[] args, Function funObj) throws SQLException {
         DBConnection con = (DBConnection) me;
@@ -169,7 +131,19 @@ public class DBConnection extends ScriptableObject {
         Object[] args_new = { con, Context.toString(args[0]) };
         return cx.newObject(me, "DBPreparedStatement", args_new);
     }
-
+    // endregion 
+    // region Private Members ...
+    // --------------------------------------------------------------------------------
+    protected Connection getConnection() {
+        return this.connection;
+    }    
+    
+    AbstractDatabase    databaseThing   = null;
+    Connection          connection      = null;
+    
+    // endregion 
+    // region Test Services for Development only ...  
+    // --------------------------------------------------------------------------------
     @JSFunction
     public static Object argTest(Context cx, Scriptable me, Object[] args, Function funObj) throws SQLException {
         if (args.length < 1)
@@ -196,9 +170,5 @@ public class DBConnection extends ScriptableObject {
         }
         return json;
     }
-
-    /** Some private data for this class. */
-    AbstractDatabase databaseThing = null;
-    Connection connection = null;
-    Boolean autoCommit = false;
+    // endregion 
 }
