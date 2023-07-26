2d8d8193001d742cc3a984f833bb070474edf486
Andreas Tondorf <atondorf@sig.biz>
2023-07-26 19:11:41 +0200

    Minor modifications to SysUtils ...

--- extension/src/main/java/twx/core/utils/SysUtilServices.java
+++ extension/src/main/java/twx/core/utils/SysUtilServices.java
@@ -1,22 +1,29 @@
 package twx.core.utils;
 
 import java.lang.reflect.Field;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.slf4j.Logger;
 
 import com.thingworx.common.utils.MonitoredThreadPoolExecutor;
 import com.thingworx.data.util.InfoTableInstanceFactory;
+import com.thingworx.entities.utils.EntityUtilities;
 import com.thingworx.logging.LogUtilities;
 import com.thingworx.metadata.annotations.ThingworxServiceDefinition;
 import com.thingworx.metadata.annotations.ThingworxServiceResult;
+import com.thingworx.relationships.RelationshipTypes.ThingworxRelationshipTypes;
 import com.thingworx.resources.Resource;
+import com.thingworx.system.subsystems.Subsystem;
 import com.thingworx.system.subsystems.eventprocessing.EventProcessingSubsystem;
 import com.thingworx.system.subsystems.eventprocessing.EventRouter;
 import com.thingworx.things.events.ThingworxEvent;
 import com.thingworx.types.InfoTable;
+import com.thingworx.types.collections.ValueCollection;
+import com.thingworx.types.primitives.StringPrimitive;
 
 public class SysUtilServices extends Resource {
     private static final long serialVersionUID = -8659764339606932258L;
@@ -56,8 +63,8 @@ public class SysUtilServices extends Resource {
         InfoTable   resulTab = InfoTableInstanceFactory.createInfoTableFromDataShape( ThingworxEvent.getDataShape() );
         
         // query the queue of EventProcessingSubsystem ... 
-        BlockingQueue<Runnable> evQueue = this.getEventQueue();
-        for (Runnable evInstance : evQueue) {
+        BlockingQueue<Runnable> evq = this.getEventQueue();
+        for (Runnable evInstance : evq) {
             // queue contains <EventInstance> which implements runnable ... 
             // but, we want the "private final ThingworxEvent event;"
             // only way to get it is reflection ...
@@ -77,17 +84,101 @@ public class SysUtilServices extends Resource {
         Long ret = (long) 0;
         // query the queue of EventProcessingSubsystem ... 
         BlockingQueue<Runnable> evq = this.getEventQueue();
-        Iterator<Runnable> it = evq.iterator();
-        while ( it.hasNext() ) {
-            Runnable r = it.next();
-            if ( r.getClass().getSimpleName() == "EventInstance" ) {
-                it.remove();
-                ret++;           
+        try {
+            Iterator<Runnable> it = evq.iterator();
+            while (it.hasNext()) {
+                Runnable r = it.next();
+                if ( r.getClass().getSimpleName().equals("EventInstance") ) {
+                    it.remove();
+                    ret++;
+                }
             }
+        } catch (ConcurrentModificationException fallThrough) {
+            // Take slow path if we encounter interference during traversal.
+            // Make copy for traversal and call remove for cancelled entries.
+            // The slow path is more likely to be O(N*N).
+            for (Object r : evq.toArray())
+                if ( r.getClass().getSimpleName().equals("EventInstance") ) {
+                    evq.remove(r);
+                    ret++;
+                }
         }
 		return ret;
 	}
 
+    /*
+    @ThingworxServiceDefinition(name = "GetEventQueueContent", description = "", category = "", isAllowOverride = false, aspects = {
+			"isAsync:false" })
+	@ThingworxServiceResult(name = "Result", description = "", baseType = "INFOTABLE", aspects = {
+			"isEntityDataShape:true", "dataShape:EventProcessingSubsystemQueue" })
+	public InfoTable GetEventQueueContent() throws Exception {
+		_logger.trace("Entering Service: GetEventQueueContent");
+		Subsystem evproc = (Subsystem) EntityUtilities.findEntity("EventProcessingSubsystem",
+				ThingworxRelationshipTypes.Subsystem);
+		EventProcessingSubsystem ev = EventProcessingSubsystem.getSubsystemInstance();
+		Class cls = ev.getClass();
+		Field workerQueue = cls.getDeclaredField("eventRouter");
+		workerQueue.setAccessible(true);
+		EventRouter eventRouter = (EventRouter)workerQueue.get(ev);
+		Class erClass = eventRouter.getClass();
+		Field mtp = erClass.getDeclaredField("workerPool");
+		mtp.setAccessible(true);
+		MonitoredThreadPoolExecutor mtpe = (MonitoredThreadPoolExecutor)mtp.get(eventRouter);
+		BlockingQueue<Runnable> br = mtpe.getQueue();
+		
+		InfoTable iftbl_Status = InfoTableInstanceFactory
+				.createInfoTableFromDataShape("EventProcessingSubsystemQueue");
+		for (Object obj: br)
+		{
+			Class cls2 = obj.getClass();
+			Field fld = cls2.getDeclaredField("event");
+			fld.setAccessible(true);
+			ThingworxEvent tve = (ThingworxEvent)fld.get(obj);
+		
+			ValueCollection vc = new ValueCollection();
+			vc.put("eventName", new StringPrimitive(tve.getEventName()));
+			vc.put("eventSource", new StringPrimitive(tve.getSource()));
+			vc.put("eventSourceProperty", new StringPrimitive(tve.getSourceProperty()));
+			
+			iftbl_Status.addRow(vc);
+		}
+		
+		_logger.trace("Exiting Service: GetEventQueueContent");
+		return iftbl_Status;
+	}
+
+	@ThingworxServiceDefinition(name = "ClearEventQueue", description = "", category = "", isAllowOverride = false, aspects = {
+			"isAsync:false" })
+	@ThingworxServiceResult(name = "Result", description = "", baseType = "STRING", aspects = {})
+	public String ClearEventQueue() throws Exception {
+		_logger.trace("Entering Service: ClearEventQueue");
+//		
+//		Subsystem evproc = (Subsystem) EntityUtilities.findEntity("EventProcessingSubsystem",
+//				ThingworxRelationshipTypes.Subsystem);
+//		EventProcessingSubsystem ev = EventProcessingSubsystem.getSubsystemInstance();
+//		Class cls = ev.getClass();
+//		Field workerQueue = cls.getDeclaredField("eventRouter");
+//		workerQueue.setAccessible(true);
+//		EventRouter eventRouter = (EventRouter)workerQueue.get(ev);
+//		Class erClass = eventRouter.getClass();
+//		Field mtp = erClass.getDeclaredField("workerPool");
+//		mtp.setAccessible(true);
+//		MonitoredThreadPoolExecutor mtpe = (MonitoredThreadPoolExecutor)mtp.get(eventRouter);
+//		BlockingQueue<Runnable> br = mtpe.getQueue();
+//		br.clear();
+		
+	
+		
+		_logger.trace("Exiting Service: GetEventQueueContent");
+	
+		_logger.trace("Exiting Service: ClearEventQueue");
+		//return Integer.toString(br.size());
+		return "";
+
+	}
+
     // endregion
+
+*/
 
 }
