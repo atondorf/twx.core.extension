104c075f5b1d0d988f83db25a6610f32e98f9a06
Andreas Tondorf <atondorf@sig.biz>
2023-11-02 14:09:40 +0100

    Further Doc

---
+++ app/src/main/java/twx/core/LiquiTest.java
@@ -0,0 +1,16 @@
+package twx.core;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.Scope;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import java.sql.Connection;
+
+public class LiquiTest {
+    
+}
